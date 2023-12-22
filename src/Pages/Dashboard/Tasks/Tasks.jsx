e4fd2a4fbbbc0114022d1e666ee9fbe5beb399d7
MosaddekHossen <mosaddekhossen7496@gmail.com>
2023-12-22 19:54:16 +0600

    final commit

--- src/Pages/Dashboard/Tasks/Tasks.jsx
+++ src/Pages/Dashboard/Tasks/Tasks.jsx
@@ -13,24 +13,30 @@ const Tasks = () => {
                 <Pane key="0" defaultSize={{ width: '33%', height: 120 }}>
                     <div className="card bg-base-100 shadow-xl">
                         <div className="card-body">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <h2 className="card-title">Daily stand-up</h2>
+                            <p>Provide updates on current tasks</p>
+                            <h2 className="card-title">2023-01-08</h2>
+                            <h2 className="card-title">Low</h2>
                         </div>
                     </div>
                 </Pane>
                 <Pane key="1" defaultSize={{ width: '33%', height: 120 }}>
                     <div className="card bg-base-100 shadow-xl">
                         <div className="card-body">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <h2 className="card-title">Client call</h2>
+                            <p>Discuss project requirements with the client</p>
+                            <h2 className="card-title">2023-01-12</h2>
+                            <h2 className="card-title">High</h2>
                         </div>
                     </div>
                 </Pane>
                 <Pane key="2" defaultSize={{ width: '33%', height: 120 }}>
                     <div className="card bg-base-100 shadow-xl">
                         <div className="card-body">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <h2 className="card-title">Research new technologies</h2>
+                            <p>Explore and learn about upcoming technologies</p>
+                            <h2 className="card-title">2022-01-08</h2>
+                            <h2 className="card-title">Moderate</h2>
                         </div>
                     </div>
                 </Pane>
