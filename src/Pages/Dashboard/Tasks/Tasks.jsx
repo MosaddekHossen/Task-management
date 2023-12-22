c4d6d8bc5b93704a166cc167d404b550faa01adb
MosaddekHossen <mosaddekhossen7496@gmail.com>
2023-12-22 18:05:24 +0600

    next commit

--- src/Pages/Dashboard/Tasks/Tasks.jsx
+++ src/Pages/Dashboard/Tasks/Tasks.jsx
@@ -4,14 +4,18 @@ const Tasks = () => {
 
     return (
         <div>
-            <SortablePane direction="horizontal" margin={20} defaultOrder={['0', '1', '2']}>
+            <div className="flex justify-between font-bold md:text-4xl text-2xl">
+                <h1>To-do</h1>
+                <h1>OnGoing</h1>
+                <h1>Completed</h1>
+            </div>
+            <SortablePane className='mt-5' direction="horizontal" margin={20} defaultOrder={['0', '1', '2']}>
                 <Pane key="0" defaultSize={{ width: '33%', height: 120 }}>
                     <div className="card bg-base-100 shadow-xl">
                         <div className="card-body">
                             <h2 className="card-title">Shoes!</h2>
                             <p>If a dog chews shoes whose shoes does he choose?</p>
                         </div>
-                        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
                     </div>
                 </Pane>
                 <Pane key="1" defaultSize={{ width: '33%', height: 120 }}>
@@ -20,7 +24,6 @@ const Tasks = () => {
                             <h2 className="card-title">Shoes!</h2>
                             <p>If a dog chews shoes whose shoes does he choose?</p>
                         </div>
-                        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
                     </div>
                 </Pane>
                 <Pane key="2" defaultSize={{ width: '33%', height: 120 }}>
@@ -29,7 +32,6 @@ const Tasks = () => {
                             <h2 className="card-title">Shoes!</h2>
                             <p>If a dog chews shoes whose shoes does he choose?</p>
                         </div>
-                        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
                     </div>
                 </Pane>
             </SortablePane>
